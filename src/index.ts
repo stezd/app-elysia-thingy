1d650a88fa79817c7d0f92ad8b407a04fc38da2c
Aldi Pramudya <llenndesu@gmail.com>
2025-05-13 21:46:53 +0700

    feat: added swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Elysia } from "elysia";
+import {Elysia} from "elysia";
+import {swagger} from "@elysiajs/swagger"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(6969);
+const app = new Elysia()
+    .use(swagger())
+    .get('/', ({path}) => path)
+    .get("/hello", 'Do you miss me nigga?')
+    .listen(6969);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
